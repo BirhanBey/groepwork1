648d0c24a171e36e7e29bacfe473b8838b72dc62
BirhanBey <114659714+BirhanBey@users.noreply.github.com>
2022-12-22 15:03:47 +0100

    t41-sidebar-filter (#5)

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-// navbar onclick
-const list = document.querySelectorAll(".list");
-function activeLink() {
-  list.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
-}
-list.forEach((item) => item.addEventListener("click", activeLink));

66f1965c589b95e5fdced17669f556d2e358d7a2
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2022-12-22 16:17:46 +0100

    update eventlistener active

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 // navbar onclick
-const list = document.querySelectorAll(".list");
-function activeLink() {
-  list.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
-}
-list.forEach((item) => item.addEventListener("click", activeLink));
+const navbarRef = document.querySelectorAll('.list');
+navbarRef.forEach((listItem) =>
+  listItem.addEventListener('click', (e) => {
+    navbarRef.forEach((listItem) => listItem.classList.remove('active'));
+    e.currentTarget.classList.add('active');
+  })
+);
 
 // sidebar toggle
-var sidebar = document.querySelector(".sidebar-container");
-var toggleBtn = document.querySelector(".btn-filters");
-var closeBtn = document.querySelector(".btn-close");
+var sidebar = document.querySelector('.sidebar-container');
+var toggleBtn = document.querySelector('.btn-filters');
+var closeBtn = document.querySelector('.btn-close');
 
 toggleBtn.onclick = function toggleSidebar() {
-  if (sidebar.classList.contains("active")) {
-    sidebar.classList.remove("active");
+  if (sidebar.classList.contains('active')) {
+    sidebar.classList.remove('active');
   } else {
-    sidebar.classList.add("active");
+    sidebar.classList.add('active');
   }
 };

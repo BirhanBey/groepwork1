6fec9384266e1129858a0246a0ad8a9a0c3eef04
Birhan Yorukoglu <birhanyorukoglu@gmail.com>
2022-12-22 13:16:23 +0100

    new navbar ready to go

--- js/navbar.js
+++ js/navbar.js
@@ -5,16 +5,3 @@ function activeLink() {
   this.classList.add("active");
 }
 list.forEach((item) => item.addEventListener("click", activeLink));
-
-// sidebar toggle
-var sidebar = document.querySelector(".sidebar-container");
-var toggleBtn = document.querySelector(".btn-filters");
-var closeBtn = document.querySelector(".btn-close");
-
-toggleBtn.onclick = function toggleSidebar() {
-  if (sidebar.classList.contains("active")) {
-    sidebar.classList.remove("active");
-  } else {
-    sidebar.classList.add("active");
-  }
-};

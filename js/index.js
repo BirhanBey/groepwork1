8f78a17684620a218b91aeee540bae8b876bf54e
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2023-01-02 13:27:17 +0100

    change index.html to index.php (#9)
    thans man. very nice.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import '../css/style.scss';
+console.log('test');
+
 /**
  * navbar
  */
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default {
+  build: {
+    manifest: true,
+    watch: true,
+    rollupOptions: {
+      input: '/js/index.js',
+    },
+  },
+};

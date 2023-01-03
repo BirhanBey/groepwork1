754c8c5b7a4fc0b47b00821fc110d1738947cc89
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2023-01-03 17:36:00 +0100

    comment image-box icon

--- index.php
+++ index.php
@@ -155,6 +155,10 @@ $manifestObject = json_decode($manifest, true);
               <a href="<?= $product["url"] ?>" target="_blank">
                 <span><i class="fa-solid fa-link"></i></span>
               </a>
+              <!-- <a style="margin-left: 10px" href="#"> -->
+              <!-- <button type="button" onclick="openModal(1)"><span>
+                  <i class="fa-regular fa-images"></i></span></button> -->
+              <!-- </a> -->
             </div>
           </div>
         </div>

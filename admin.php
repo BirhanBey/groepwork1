372b4cb09c1ed4d85b089a2a1ea9676767b4f9a4
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2023-01-08 13:46:57 +0100

    T85 data to add product (#43)
    * move admin.php to root folder
    
    * add select fields for color, brand and category
    
    * remove console.log

--- admin.php
+++ admin.php
@@ -1,6 +1,9 @@
 <?php
-$manifest = file_get_contents("../dist/manifest.json");
+$manifest = file_get_contents("./dist/manifest.json");
 $manifestObject = json_decode($manifest, true);
+
+require './data/categories.php';
+require './data/filters.php';
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +14,8 @@ $manifestObject = json_decode($manifest, true);
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin Page</title>
-  <link rel="stylesheet" href="../dist/<?= $manifestObject["js/admin.css"]["file"] ?>">
-  <script src="../dist/<?= $manifestObject["js/admin.js"]["file"] ?>" defer></script>
+  <link rel="stylesheet" href="./dist/<?= $manifestObject["js/admin.css"]["file"] ?>">
+  <script src="./dist/<?= $manifestObject["js/admin.js"]["file"] ?>" defer></script>
 </head>
 
 <body>
@@ -22,7 +25,7 @@ $manifestObject = json_decode($manifest, true);
       <li class="list">
         <b></b>
         <b></b>
-        <a href="../index.php">
+        <a href="./index.php">
           <span class="icon"><i class="fa-solid fa-house-user"></i></span>
           <span class="title">Home</span>
         </a>
@@ -129,12 +132,34 @@ $manifestObject = json_decode($manifest, true);
         <div class="info info5">
           <i class="fa-solid fa-boxes-stacked icon"></i>
           <label for="categories">Categories</label><br />
-          <input type="text" id="categories" name="fav_language" placeholder="  Categories" />
+          <select id="categories" name="fav_language" multiple>
+            <option value="" selected disabled hidden>choose a or multiple categories</option>
+
+            <?php foreach ($categories as $category) { ?>
+              <option value="<?= $category["name"] ?>"><?= $category["name"] ?></option>
+              <?php if ($category["subcategories"]) { ?>
+                <optgroup label="<?= $category["name"] ?>">
+
+                  <?php foreach ($category["subcategories"] as $subcategory) { ?>
+                    <option value="<?= $category["name"] . "-" . $subcategory["name"] ?>"><?= $subcategory["name"] ?></option>
+              <?php
+                  }
+                }
+              } ?>
+
+          </select>
         </div>
         <div class="info info6">
           <i class="fa-solid fa-palette icon"></i>
           <label for="color">Color</label><br />
-          <input type="text" id="color" name="fav_language" placeholder="  Color" />
+          <select id="color" name="fav_language">
+            <option value="" selected disabled hidden>choose a color</option>
+
+            <?php foreach ($colors as $color) { ?>
+              <option value="<?= $color["name"] ?>"><?= $color["name"] ?></option>
+            <?php } ?>
+
+          </select>
         </div>
         <div class="info info7">
           <i class="fa-solid fa-euro-sign icon"></i>
@@ -144,16 +169,21 @@ $manifestObject = json_decode($manifest, true);
         <div class="info info8">
           <i class="fa-regular fa-copyright icon"></i>
           <label for="brand">Brand</label><br />
-          <input type="text" id="brand" name="fav_language" placeholder="  Brand" />
-        </div>
+          <select id="brand" name="fav_language">
+            <option value="" selected disabled hidden>choose a brand</option>
 
+            <?php foreach ($brands as $brand) { ?>
+              <option value="<?= $brand["name"] ?>"><?= $brand["name"] ?></option>
+            <?php } ?>
+
+          </select>
+        </div>
         <div class="info info9">
           <i class="fa-solid fa-link icon"></i>
           <label for="url">URL</label><br />
           <input type="text" id="url" name="fav_language" placeholder="  URL" />
         </div>
         <button type="submit" class="add-item">Add Item</button>
-
       </form>
       <button class="close-addnew">
         <i class="fa-regular fa-circle-xmark"></i>

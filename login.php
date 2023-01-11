8b0bd85d5550f0e52511a835b2d3e9e82c4ec6a5
Cyril Cornez <77747225+CCornez@users.noreply.github.com>
2023-01-11 20:12:51 +0100

    delete php scripts

--- login.php
+++ login.php
@@ -1,14 +1,6 @@
 <?php
 $manifest = file_get_contents("./dist/manifest.json");
 $manifestObject = json_decode($manifest, true);
-
-
-$errors = [
-    "foutje",
-    "niet rechtstreeks naar auth surfen aub!",
-    "velden mogen niet leeg zijn",
-    "username en password zijn niet juist"
-];
 ?>
 <!doctype html>
 <html lang="en">
@@ -17,7 +9,6 @@ $errors = [
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="./dist/<?= $manifestObject["js/login.css"]["file"] ?>">
     <script src="./dist/<?= $manifestObject["js/login.js"]["file"] ?>" defer></script>
 </head>
@@ -25,17 +16,12 @@ $errors = [
 <body>
     <div class="login">
         <h1>Login</h1>
-        <?php
-        if (isset($_GET["error"])) {
-            print $errors[$_GET["error"]];
-        }
-        ?>
         <form method="post" action="auth.php">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" value="" placeholder="Password">
-            <button type="submit" class="btn btn-primary">Login</button>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Username">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password">
+            <button type="submit">Login</button>
         </form>
     </div>
 </body>
